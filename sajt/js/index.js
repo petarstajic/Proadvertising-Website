7a23276e531fe1f7690a904357984e5636bfc26f
Petar Stajic <petarstajic91@gmail.com>
2020-07-07 14:57:25 +0200

    Initial commit

---
+++ sajt/js/index.js
@@ -0,0 +1,35 @@
+$(function(){ 
+$("#panel-tabs1").hover(
+ function() {
+    $('#demo').collapse('show');
+  }, function() {
+    $('#demo').collapse('hide');
+  }
+);
+
+
+
+$("#panel-tabs2").hover(
+ function() {
+    $('#demo2').collapse('show');
+  }, function() {
+    $('#demo2').collapse('hide');
+  }
+);
+
+$("#panel-tabs3").hover(
+ function() {
+    $('#demo3').collapse('show');
+  }, function() {
+    $('#demo3').collapse('hide');
+  }
+);
+
+$("#panel-tabs4").hover(
+ function() {
+    $('#demo4').collapse('show');
+  }, function() {
+    $('#demo4').collapse('hide');
+  }
+);
+});
